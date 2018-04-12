61318cc1f2ddbabea389caddd208ad8dba31a6be
junxiong96 <37995428+junxiong96@users.noreply.github.com>
2018-04-12 10:21:37 +0800

    Update redemptgift.blade.php

--- resources/views/redemptgift.blade.php
+++ resources/views/redemptgift.blade.php
@@ -227,39 +227,14 @@
       <div class="col-sm-12">
         <form id="promoForm" name="sentMessage" novalidate>
           <div class="row">
+            @foreach($data as $img)
             <div class="col-sm-4">
               <div class="form-group">
-                  <img src="https://s4.thcdn.com/productimg/0/600/600/88/10626288-1343052865-451265.jpg" width="200px" height="200px">
+              <img src="{{url($img->Web_Path)}}" width="200px" height="200px">
               </div>
+              
             </div>
-            <div class="col-sm-4">
-              <div class="form-group">
-                  <img src="https://1uhocf2skxl2mzfok4cuutlqf1-wpengine.netdna-ssl.com/wp-content/uploads/2012/09/NARS-Andy-Warhol-Larger-Than-Life-Lip-Gloss-group-set.jpg" width="200px" height="200px">
-              </div>
-            </div>
-            <div class="col-sm-4">
-              <div class="form-group">
-                  <img src="https://s1.thcdn.com/productimg/0/960/960/24/10999824-1413991019-744584.jpg" width="200px" height="200px">
-              </div>
-            </div>
-            <div class="col-sm-4">
-              <div class="form-group">
-                  <img src="http://www.narscosmetics.com/on/demandware.static/-/Sites-itemmaster_NARS/default/dw3d08a39b/hi-res/0607845051572.jpg" width="200px" height="200px">
-              </div>
-            </div>
-            <div class="col-sm-4">
-              <div class="form-group">
-                  <img src="https://d1ibm7nofgdn3g.cloudfront.net/nars-cosmetics-duo-eyeshadow-kuala-lumpur-1101822.jpg" width="200px" height="200px">
-              </div>
-            </div>
-            <div class="col-sm-4">
-              <div class="form-group">
-                  <img src="https://s4.thcdn.com/productimg/0/960/960/23/10999823-1413991019-744820.jpg" width="200px" height="200px">
-              </div>
-            </div>
-
-
-
+            @endforeach
             <div class="clearfix"></div>
           </div>
         </form>
@@ -293,7 +268,7 @@
   
   var data = [
     @foreach($data as $data)                            
-        {"label":"{{$data->DCS_CODE}}/{{$data->ALU}}", "value":"{{$data->ALU}}",  "DCS":"{{$data->DCS_CODE}}"},
+        {"label":"{{$data->Name}}", "value":"{{$data->ALU}}",  "DCS":"{{$data->DCS_CODE}}"},
     @endforeach                             
   ];
 
