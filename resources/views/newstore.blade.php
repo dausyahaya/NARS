1b87b9f0e774c2606625316878ec924f87bdf346
Firdaus Yahaya <37564341+dausyahaya@users.noreply.github.com>
2018-04-12 11:45:10 +0800

    Update newstore.blade.php

--- resources/views/newstore.blade.php
+++ resources/views/newstore.blade.php
@@ -1,6 +1,33 @@
 @extends('layouts.app1')
-
+@section('head')
+<style>
+.sumtable{
+  background-color:#fff;
+  font-size: 12px;
+  margin-left:15%;
+  margin-right:15%;
+  color:#fff;
+  width:100%;
+  height:300px;
+}
+table{
+  background:#fff;
+}
+th, td {
+    text-align: left;
+    padding: 8px;
+    background:#fff;
+}
+tr:nth-child(even){background-color: #f2f2f2}
+</style>
+@endsection
 @section('content')
+
+<script>
+$(document).ready(function(){
+  $('#quantity-table').DataTable();
+});
+</script>
 
     <section id="admin">
       <div class="container">
@@ -37,11 +64,12 @@
             </form>
           </div>
 
+          <div class="container">
           <div class="col-lg-12">
-            <form id="" name="">
+            <form id="promoForm" name="sentMessage" novalidate>
               <div class="row">
-                <div class="wrapper" style="overflow-x:scroll;overflow-y:scroll;background:#fff;margin: 0 auto;">
-                    <table class="table table-striped sumtable" style="width:800px;">
+                <div class="wrapper" style="background:#fff;margin: 0 auto;">
+                    <table class="table table-striped sumtable" id='quantity-table'>
                       <thead>
                         <tr>
                           <th>NO</th>
@@ -66,13 +94,23 @@
 
                       </tbody>
                     </table>
+                  </div>
+
+
                 </div>
-              </div>
-            </form>
+              </form>
+            </div>
           </div>
 
+        </br></br>
+        &nbsp;
+
+            <div class="col-lg-12 text-center">
+              <div id="success"></div>
+              <a href='{{ URL::to("/catredeemsummary")}}'><button id="backbutton" class="btn btn-primary btn-xl text-uppercase" type="button">BACK</button></a>
+            </div>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
 
 @endsection
